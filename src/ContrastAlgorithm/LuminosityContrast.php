9c9edc9b1b75777cbe702eded0d7fef720ff98d4
David Gorges <david.gorges@leaphub.de>
2015-03-01 13:13:22 -0800

    refactor(ColorContrast): improve readability

--- src/ContrastAlgorithm/LuminosityContrast.php
+++ src/ContrastAlgorithm/LuminosityContrast.php
@@ -3,34 +3,47 @@
 namespace ColorContrast\ContrastAlgorithm;
 
 use MischiefCollective\ColorJizz\ColorJizz;
+use MischiefCollective\ColorJizz\Formats\RGB;
 
 /**
  * The LuminosityContrast calculates the contrast based on the proposed
- * algorithm from the WCAG 2.0 guides
+ * algorithm from the WCAG 2.0 guides (ITU-R Recommendation BT. 709)
  *
  * @link http://www.w3.org/TR/WCAG20/#contrast-ratiodef
  */
 class LuminosityContrast implements ContrastAlgorithmInterface
 {
     /**
-     * @inheritdoc
+     * returns the contrast ratio between foreground and background color.
+     *
+     * @param ColorJizz $foreground
+     * @param ColorJizz $background
+     *
+     * @return float
      */
     public function calculate(ColorJizz $foreground, ColorJizz $background)
     {
-        $c1 = $background->toRGB();
-        $c2 = $foreground->toRGB();
-        $luminosity1 = 0.2126 * pow($c1->getRed() / 255, 2.2) +
-            0.7152 * pow($c1->getGreen() / 255, 2.2) +
-            0.0722 * pow($c1->getBlue() / 255, 2.2);
+        $fgLuma  = $this->relativeLuminosity($foreground->toRGB());
+        $bgLuma  = $this->relativeLuminosity($background->toRGB());
 
-        $luminosity2 = 0.2126 * pow($c2->getRed() / 255, 2.2) +
-            0.7152 * pow($c2->getGreen() / 255, 2.2) +
-            0.0722 * pow($c2->getBlue() / 255, 2.2);
-
-        if ($luminosity1 > $luminosity2) {
-            return ($luminosity1 + 0.05) / ($luminosity2 + 0.05);
+        if ($fgLuma > $bgLuma) {
+            return ($fgLuma + 0.05) / ($bgLuma + 0.05);
         } else {
-            return ($luminosity2 + 0.05) / ($luminosity1 + 0.05);
+            return ($bgLuma + 0.05) / ($fgLuma + 0.05);
         }
+    }
+
+    /**
+     * @param RGB $color
+     *
+     * @return float
+     */
+    private function relativeLuminosity(RGB $color)
+    {
+        $luminosity = 0.2126 * pow($color->getRed() / 255, 2.2) +
+            0.7152 * pow($color->getGreen() / 255, 2.2) +
+            0.0722 * pow($color->getBlue() / 255, 2.2);
+
+        return $luminosity;
     }
 }
